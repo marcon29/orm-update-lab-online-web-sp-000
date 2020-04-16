ac55c56732eaeb38434a16539ccc6952d8d28a65
marcon29 <online@markstabler.com>
2020-04-16 10:54:05 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -13,6 +13,63 @@ class Student
     @grade = grade
   end
 
-  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE students"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade) 
+        VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+
+  def update
+    sql = <<-SQL
+      UPDATE students
+      SET name=?, grade=?
+      WHERE id=?
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+
+  def self.new_from_db(record)
+    Student.new(record[0], record[1], record[2])
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE name=?
+      LIMIT 1
+    SQL
+    DB[:conn].execute(sql, name).collect { |record| self.new_from_db(record) }.first
+  end
+
+
 
 end
